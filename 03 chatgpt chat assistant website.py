2f5194f71426b0d593f23bd93dea5ba2787e14da
ruthwika2003 <158016646+ruthwika2003@users.noreply.github.com>
2024-01-29 18:47:02 +0530

    Update 03 chatgpt chat assistant website.py

--- 03 chatgpt chat assistant website.py
+++ 03 chatgpt chat assistant website.py
@@ -1,7 +1,7 @@
 import openai
 import gradio
 
-openai.api_key = "####"
+openai.api_key = "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"
 
 messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
 
