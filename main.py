77aab399da6b45c88ccd33e223532d02d3bcdfd4
ruthwika2003 <158016646+ruthwika2003@users.noreply.github.com>
2024-01-30 11:55:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import json
 from difflib import get_close_matches
 
 # Load the knowledge base
-def load_knowledge_base(C:\Users\ruthw\Downloads\Mental_Health_FAQ.csv: str) -> dict:
-  with open(C:\Users\ruthw\Downloads\Mental_Health_FAQ.csv, 'r') as file:
+def load_knowledge_base(Mental_Health_FAQ.csv: str) -> dict:
+  with open(Mental_Health_FAQ.csv, 'r') as file:
     data: dict = json.load(file)
   return data
 
 
-def save_knowledge_base(C:\Users\ruthw\Downloads\Mental_Health_FAQ.csv: str, data: dict):
-  with open(file_path, 'w') as file:
+def save_knowledge_base(Mental_Health_FAQ.csv: str, data: dict):
+  with open(Mental_Health_FAQ.csv, 'w') as file:
     json.dump(data, file, indent=2)
