932bd54d8b5356884690d725b56c141b17124be9
ruthwika2003 <158016646+ruthwika2003@users.noreply.github.com>
2024-03-08 17:35:19 +0530

    created test folder

--- demo_1.py
+++
@@ -1,6 +0,0 @@
-import openai
-
-openai.api_key = "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"
-
-completion = openai.ChatCompletion.create(model="gpt-3.5", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
-print(completion.choices[0].message.content)
