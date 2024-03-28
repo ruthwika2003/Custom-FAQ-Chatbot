d8ab96cdee47016e99958b9d72e5993db33a197e
ruthwika2003 <158016646+ruthwika2003@users.noreply.github.com>
2024-03-28 10:18:26 +0530

    changed the model

--- Ollama_bot/main.py
+++ Ollama_bot/main.py
@@ -8,9 +8,11 @@ headers = {
     'Content-type': 'application/json',
 }
 
+# BASIC BOT
+
 def generate_response():
     data = {
-        "model": "mistral",
+        "model": "llama2:7b",
         "prompt": "Why is mental health important?",
         "stream": False
     }
@@ -26,11 +28,35 @@ def generate_response():
     else:
         print("Error:", response.status_code, response.text)
         return None
+
+# CUSTOM BOT
+
+urls = [
+    'https://www.medicalnewstoday.com/articles/73936'
+    'https://www.totalhealthandfitness.com/what-is-mindful-eating-your-complete-guide-to-enjoying-your-food-more/'
+    'https://www.totalhealthandfitness.com/nutrition-and-chronic-disease/'
+    'https://www.glofox.com/blog/fitness-blogs/'
+]
+
+def answer():
+    for url in urls:
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            if text:
+                return f"{text[0]}"
+        
+        except Exception as e:
+            print(f"Error processing URL : {e}")
     
+    return f"Sorry I couldn't find the answer to your questions."
+    
+
 iface = gr.Interface(
-    fn=generate_response(),
+    fn=generate_response,
     inputs=gr.Textbox(lines=2, placeholder="Enter your prompt here..."),
     outputs="text"
 )
+
 
 iface.launch()
